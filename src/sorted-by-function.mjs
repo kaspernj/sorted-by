b44cec5e38377bbf24b2919e882d7d01005586d2
kaspernj <k@spernj.org>
2024-07-16 10:15:19 +0000

    sortedByFunction: Fixed integers converted to strings

--- src/sorted-by-function.mjs
+++ src/sorted-by-function.mjs
@@ -1,12 +1,17 @@
+const isNull = (value) => value === undefined || value === null
+
 const sortedByFunction = (array, functionName) => {
-  const isNull = (value) => value === undefined || value === null
-
   return array.sort((item1, item2) => {
-    const value1 = `${item1[functionName]()}`
-    const value2 = `${item2[functionName]()}`
+    const value1 = item1[functionName]()
+    const value2 = item2[functionName]()
 
-    if (isNull(value1) && !isNull(value2)) return -1
-    if (!isNull(value1) && isNull(value2)) return 1
+    if (isNull(value1) && !isNull(value2)) {
+      return -1
+    }
+
+    if (!isNull(value1) && isNull(value2)) {
+      return 1
+    }
 
     if (typeof value1 == "string" && typeof value2 == "string") {
       return value1.localeCompare(value2)
